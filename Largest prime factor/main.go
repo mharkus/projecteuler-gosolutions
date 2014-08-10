6199b4a84a75ae4d29818c8a2cfa43155e8f1591
Marc Lester Tan <mailtan@gmail.com>
2014-08-10 23:32:56 +0800

    added Largest Prime Factor solution

---
+++ Largest prime factor/main.go
@@ -0,0 +1,48 @@
+package main
+
+import(
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func solve(index, val, largestPF int){
+
+	if index > val{
+		fmt.Println("Largest prime factor is ",largestPF)
+		return
+	}
+
+	if val % index == 0{
+		if index > largestPF {
+			largestPF = index
+		}
+
+		fmt.Println("prime: ", index)
+
+		val = val / index
+
+		solve(index, val, largestPF)
+	}else{
+		solve(index + 1, val, largestPF)
+	}
+
+
+}
+
+func main() {
+
+	if len(os.Args) == 1{
+		fmt.Println("\nPrints the largest prime factor using brute-force method.\nUsage: go run main.go NUM\n")
+		return
+	}
+
+	param, err := strconv.Atoi(os.Args[1]);
+	if err != nil {
+		fmt.Println("\nPlease input numeric value only.\n")
+		return
+	}
+
+	solve(2, param , 0)
+}
+
