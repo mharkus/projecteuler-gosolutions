d3a719f403c42c4e5200c3fc13d900d59ab864bb
Marc Lester Tan <mailtan@gmail.com>
2014-08-11 00:38:25 +0800

    added Largest Palindrome Product

---
+++ Largest Palindrome Product/main.go
@@ -0,0 +1,49 @@
+package main
+
+import(
+	"fmt"
+	"strconv"
+	"unicode/utf8"
+)
+
+func solve(x, y, largest int){
+	if x < 100 && y  < 100 {
+		fmt.Printf("largest palindrome %d\n", largest)
+		return
+	}else if x < 100 {
+		solve(999, y - 1, largest)
+		return
+	}
+
+	product := x * y
+	if(isPalindrome(product)){
+		
+		if largest < product {
+			largest = product
+		}
+	}
+
+	solve(x - 1, y, largest)
+}
+
+func isPalindrome(num int) bool {
+	val := strconv.Itoa(num)
+	return val == reverse(val)
+}
+
+func reverse(s string) string{
+	cs := make([]rune, utf8.RuneCountInString(s))
+	i := len(cs)
+
+	for _, c := range s {
+		i--
+		cs[i] = c
+	}
+
+	return string(cs)
+}
+
+func main() {
+	solve(999, 999, 0)
+}
+
